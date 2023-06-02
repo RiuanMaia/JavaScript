8b3d1b94ef6e5c77f154cbc3278ebeac232d31ae
Riuan Maia <riuanmaia10@gmail.com>
2023-06-02 14:59:45 -0300

    Atualização no js da pokedex, falta inputar os pokemons a lista

--- PokemómList/assets/JavaScript/script.js
+++ PokemómList/assets/JavaScript/script.js
@@ -1,24 +1,42 @@
 const buttonStart = document.querySelector(".start");
 const pokeInfo = document.querySelector(".pokeInfo");
-const pokeImg = document.querySelector(".pokeImg img");
+const pokeImgScreen = document.querySelector(".pokeImg");
+let pokeImg = document.querySelector(".pokeImg img");
 let pokeName = document.querySelector(".pokeImg h1");
 let region = document.querySelector(".regiao");
 let type = document.querySelector(".tipo");
+let description = document.querySelector(".description");
 let pokemonList = [
-    {name:"squirtle", region: "kanto", type: "água", description: "Squirtle é um pokemon destemido e amavel, cuidado com seu jato de água!", img: "../images/squirtle.png"},
+    {name:"squirtle", region: "kanto", type: "água", description: "Squirtle é um pokemon destemido e amavel, cuidado com seu jato de água!", img: "../images/squirtle.jpg"},
+
+    {name:"pikachu", region: "kanto", type: "Elétrico", description: "pikachu é um pokemon tipo elétrico muito audaz, cuidado com seu choque do trovão!", img: "../images/squirtle.jpg"}
 ]
-let pokemon = 0;
+let pokemon = 1;
 
 buttonStart.addEventListener("click",start);
 
 function start(){
-    renderInfo();
+    if(pokeImgScreen.style.display !== "block"){
+        insertInfo();
+        renderInfo();
+    }else{
+        pokeImgScreen.style.display = "none";
+        pokeInfo.style.display = "none";
+    }
+    
 }
-function renderInfo(){
+function insertInfo(){
     region.innerHTML = pokemonList[pokemon].region;
 
     type.innerHTML = pokemonList[pokemon].type;
 
     pokeName.innerHTML = pokemonList[pokemon].name;
 
+    pokeImg.src = pokemonList[pokemon].img;
+
+    description.innerHTML = pokemonList[pokemon].description
+}
+function renderInfo(){
+    pokeImgScreen.style.display = "block";
+    pokeInfo.style.display = "block";
 }
