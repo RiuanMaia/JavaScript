60cf9d939bab58414abb15704a5afe4184736164
Riuan Maia <riuanmaia10@gmail.com>
2023-06-07 13:25:56 -0300

    Types em retornos de função

--- TypeScript/EstudoTS/src/script.ts
+++ TypeScript/EstudoTS/src/script.ts
@@ -28,11 +28,31 @@ nomes.push(89);
 //--noEmitOnError
 let nomes: any[] = ["Riuan", "Anderson", "Robson", "Leudo"];
 nomes.push(89);
-*/
+
 
 //usando types nos parametros de uma função:
-function FirstLetterUpperCase(name){
+function FirstLetterUpperCase(name: string){
    let  firstLetter = name.charAt(0).toUpperCase();
    return `${firstLetter}${name.substring(1)}`; 
 }
 FirstLetterUpperCase("riuan");
+*/
+//usando types no retorno de uma função  
+function FirstLetterUpperCase(name: string): string{
+    let  firstLetter = name.charAt(0).toUpperCase();
+    return `${firstLetter}${name.substring(1)}`; 
+ }
+ let nome: string = FirstLetterUpperCase("riuan");
+
+
+function somar(n1:number, n2:number): string {
+    return `${n1 + n2}`;
+ }
+ let soma: string = somar(6,4);
+
+//diferenças entres type de retorno
+
+ function subtrair(n1:number, n2:number): number {
+    return n1 - n2;
+ }
+ let subtrai: number = subtrair(6,4);
