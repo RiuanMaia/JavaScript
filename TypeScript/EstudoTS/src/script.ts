43d2f6a2f432f9d1bae1984a29a87344b8a3a028
Riuan Maia <riuanmaia10@gmail.com>
2023-06-08 15:38:58 -0300

    mais alguns conteúdos ts

--- TypeScript/EstudoTS/src/script.ts
+++ TypeScript/EstudoTS/src/script.ts
@@ -182,17 +182,17 @@ type Nome = "Riuan" | "riuan";
 let nome: Nome = "Riuan";
 
 function mostraTexto(texto: string, alinhamento: Alinha ) {
-    //.............
+    .............
 }
 mostraTexto(nome, "center");
 mostraTexto(nome, "right");
-*/
+
 //inferência literal
 //gerando um erro:
 //o código abaixo foi implementado para gerar um union type para ser utilizado em method resolvendo o erro já que agora method só aceita GET ou POST, e não só o parâmetro da função
 type Methods = "GET" | "POST";
 function fazerRequisicao(url: string, method: "GET" | "POST") {
-    //.............
+    .............
 }
 //independente de se utilizar objetos ou variáveis separadas, o erro ocorre no mesmo lugar,o erro ocorre por conta da facilidade de mudanças que o atributo method pode sofrer, o código em si não está errado, no momento.
 let url: string =  "https://google.com.br";
@@ -210,3 +210,78 @@ let req: Requisicao = {
     };
 fazerRequisicao(req.url,req.method);
 //isso é protejer a informação dos dois lados.
+
+//type para funções:
+//sintaxe:
+type MathFunction = (n1: number, n2: number) => number;
+
+const somar: MathFunction = (n1,n2) => {
+    return n1+n2;
+}
+const subtrair: MathFunction = (n1,n2) => {
+    return n1-n2;
+}
+const multiplica: MathFunction = (n1,n2) => {
+    return n1*n2;
+}
+const divide: MathFunction = (n1,n2) => {
+    return n1/n2;
+}
+
+//retorno void:
+function removerElemento(el: HTMLElement): void {
+    el.remove();
+}
+//um erro que foi gerado foi: o elemento de id "test" pode assumir HTMLElement ou null, e o el da função "removerElemento" não pode receber elementos null. Para resolver foi preciso fazer uma pequena verificação no elemento inputado na função;
+let test = document.getElementById("test");
+if(test !== null){
+    removerElemento( test);
+}
+//quando há um retorno que é diferente de void, a função void apresenta erro. Podemos usar um "return;" para sair de uma função se quisermos como exemplo abaixo:
+
+function adicionarClass(el: HTMLElement): void {
+    if(el.classList.contains("show")){
+        return;
+    }
+    el.classList.add("show");
+}
+let modal = document.getElementById("modal");
+if(modal !== null){
+    adicionarClass(modal);
+}
+//se não colocarmos um retorno na função ela entende como se fosse um retorno do tipo void;
+
+type QualquerFuncao = () => void;
+const algo: QualquerFuncao = () => {
+    return 21;
+}
+//quando criamos uma função que retorna void, ela obrigatoriamente precisa retornar void, mas caso seja criado um type com o retorno void e esse type seja utilizado na mesma função, ela não esperará nenhum retorno, mas se vier algum, não tem problema algum, geralmente esse retorno não vai ser utilizado.
+
+//utilizando o watchMode no arquivo:
+//para transcrevermos nosso .ts para .js utilizamos o comando: tsc src/script.ts -outDir public --noEmitOnError, pois o TS não é rodável.
+
+//utilizamos o comando "tsc src/script.ts -w" para assim sincronizarmos o nosso .ts com o .js.
+//então não precisamos ficar inputando comandos no terminal a todo momento, isso é o watchMode em ação, como se fosse um live server utilizado no HTML;
+const nome: string = "Riuan";
+*/
+//criando o arquivo tsconfig.json:
+
+//utilizamos o comando tsc --init para criar nosso arquivo
+
+//para a quantidade de arqueivos .ts que tivermos, assim que digitarmos no cmd "tsc" será criado um arquivo .js equivalente.
+
+//para os .js aparecerem na nossa pasta public o comando é: "tsc -outDir public"
+
+//mas isso seria simples, mais simples ainda é o que o arquivo tsconfig pode fazer, é o que veremos nas próximas aulas.
+
+//o importante é sabermos que estamos mexendo com todo o projeto agora.
+
+//para deixar todos os arquivos do projeto em watchMode é so utilizar o comando: "tsc -w";
+//escolhendo quais arquivos compilar:
+
+//no final do arquivo tsconfig.json colocamos um "exclude": [
+    "**/*.dev.ts"
+    //arquivos dev.ts são arquivos meramente para desenvolvimento, ou seja não precisam ser compilados.
+//]
+//esse arquivo funciona para determinarmos quem compilar.
+//quando trabalhamos com projetos, esse
